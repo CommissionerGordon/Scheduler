d947bc1f42cac56d7f4911540208db24fb41b59f
FunnyGopher <FunnyGopher@Hermes>
2015-02-21 22:04:45 -0700

    Created a premade database in the project. Updated the maven pom.xml to include the db and webapp folder. Maven will now assemble the project as well.

--- src/main/java/com/github/commissionergordon/scheduler/classes/Activity.java
+++ src/main/java/com/github/commissionergordon/scheduler/classes/Activity.java
@@ -15,7 +15,7 @@ public class Activity {
     private Date startTime;
     private int user_id;
 
-    Activity(float duration, int id, String name, int user_id){
+    public Activity(float duration, int id, String name, int user_id){
         this.duration = duration;
         this.id = id;
         this.name = name;
@@ -26,7 +26,7 @@ public class Activity {
         this.priority = 0;
     }
 
-    Activity(Activity activity){
+    public Activity(Activity activity){
         this.duration = activity.duration;
         this.enabled = activity.enabled;
         this.id = activity.id;
@@ -37,7 +37,7 @@ public class Activity {
         this.user_id = activity.user_id;
     }
 
-    Activity(float duration, boolean enabled, int id, String name, int parts, int priority, Date startTime, int user_id){
+    public Activity(float duration, boolean enabled, int id, String name, int parts, int priority, Date startTime, int user_id){
         this.duration = duration;
         this.enabled = enabled;
         this.id = id;
